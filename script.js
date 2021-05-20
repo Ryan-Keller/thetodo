cef2d7b88ba37a3537caecd8bba6cc186f216080
Ryan Keller <rjkeller_01@hotmail.com>
2021-05-19 18:45:29 -0700

    add in the middle  of the js

--- script.js
+++ script.js
@@ -1,17 +1,30 @@
 const listsContainer = document.querySelector('[data-lists]')
 const newListForm = document.querySelector('[data-new-list-form]')
 const newListInput = document.querySelector('[data-new-list-input]')
+const deleteListButton = document.querySelector('[data-delete-list-button]')
 
 const LOCAL_STORAGE_LIST_KEY = 'task.lists'
 const LOCAL_STORAGE_SELECTED_LIST_ID_KEY = 'task.selectedListId'
 let lists = JSON.parse(localStorage.getItem(LOCAL_STORAGE_LIST_KEY)) || []
 let selectedListId = localStorage.getItem(LOCAL_STORAGE_SELECTED_LIST_ID_KEY)
 
+const listDisplayContainer = document.querySelector("[data-list-display-container]")
+const listTitleElement = document.querySelector('[data-list-title]')
+const listCountElement = document.querySelector('[data-list-count]')
+const tasksContainer = document.querySelector('[data-tasks]')
+const taskTemplate = document.getElementById('task-template')
+
 listsContainer.addEventListener('click', e =>{
     if (e.target.tagName.toLowerCase() === 'li'){
         selectedListId = e.target.dataset.listId
         saveAndRender()
     }
+})
+
+deleteListButton.addEventListener('click', e=>{
+    lists = lists.filter( list => list.id !==selectedListId)
+    selectedListId = null
+    saveAndRender()
 })
 
 newListForm.addEventListener('submit', e =>{
@@ -40,6 +53,43 @@ function save(){
 
 function render(){
     clearElement(listsContainer)
+    renderLists()
+
+    const selectedList = lists.find(list => list.id === selectedListId)
+    if (selectedListId == null){
+        listDisplayContainer.style.display = 'none'
+    }else{
+        listDisplayContainer.style.display = ''
+        listTitleElement.innerText = selectedList.name
+        renderTaskCount(selectedList)
+        clearElement(tasksContainer)
+        renderTasks(selectedList)
+    }
+}
+
+function renderTasks(selectedList){
+    selectedList.tasks.forEach(task =>{
+        const taskElement = document.importNode(taskTemplate.contentEditable, true)
+        const checkBox = taskElement.querySelector('input')
+        checkBox.id = task.id
+        checkBox.checked = task.complete
+        const label = taskElement.querySelector('label')
+        label.htmlfor - task.id
+        label.append(task.name)
+        tasksContainer.appendChild(taskElement)
+    })
+}
+
+
+function renderTaskCount(selectedList){
+    const incompleteTaskCount = selectedList.tasks.filter(task =>
+        !task.complete).length
+        const taskString = incompleteTaskCount === 1 ? "task" : "tasks"
+        listCountElement.innerText = `${incompleteTaskCount} ${taskString} remaining`
+
+}
+
+function renderLists(){
     lists.forEach(list => {
         const listElement = document.createElement('li')
         listElement.dataset.listId = list.id
